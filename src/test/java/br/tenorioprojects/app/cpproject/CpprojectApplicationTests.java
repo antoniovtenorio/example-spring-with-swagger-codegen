35d8037b24a55a95cf7d952ceae0504fa447e019
Tenorio <antonio.v.tenorio@accenture.com>
2021-07-14 11:55:23 -0300

    project started

---
+++ src/test/java/br/tenorioprojects/app/cpproject/CpprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.tenorioprojects.app.cpproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
